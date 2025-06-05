0675ad70e8b7aca54bff97e602c1a864c5cd6503
Haoyu Liao <jefferyliao@linecorp.com>
2025-06-05 16:10:39 +0800

    feat: Draft Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Timer from './pages/Timer'; // Import Timer
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Timer from "./pages/Timer"; // Import Timer
+import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/timer" element={<Timer />} /> {/* Add Timer route */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/timer" element={<Timer />} /> {/* Add Timer route */}
+        </Route>
       </Routes>
     </Router>
   );
